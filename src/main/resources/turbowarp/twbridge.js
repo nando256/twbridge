0c9340c41ccaaedfa643d62e4dbdb8ff8441f0d1
nando256 <kastudio.256@gmail.com>
2025-12-04 14:23:50 +0000

    Guard crypto usage in TurboWarp extension UUID generation

--- src/main/resources/turbowarp/twbridge.js
+++ src/main/resources/turbowarp/twbridge.js
@@ -142,7 +142,7 @@
     }
 
     _uuid() {
-      if (crypto && crypto.randomUUID) return crypto.randomUUID();
+      if (typeof crypto !== 'undefined' && crypto && crypto.randomUUID) return crypto.randomUUID();
       return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
         const r = Math.random() * 16 | 0;
         const v = c === 'x' ? r : (r & 0x3 | 0x8);
